125fce524bfd6c44429eab41b561142030a4bbee
SergioAlmada <saalm56@gmail.com>
2020-06-27 12:03:40 -0400

    Better Experiment
    Better Experiment, where the improvement in convergence shows,

--- code/main.py
+++ code/main.py
@@ -6,7 +6,7 @@ torch.manual_seed(0)
 gopt = optimizationMethods(optimizationFunctionFactory.create("ackley"))
 
 print("regular gradient descent")
-x_best, y_best = gopt.gradient_descent(0.0001, 1, 1000)
+x_best, y_best = gopt.gradient_descent(0.01, 1, 1000)
 
 print("x_best ", x_best)
 print("y_best ", y_best)
@@ -14,7 +14,7 @@ print("y_best ", y_best)
 print("perturbed gradient descent")
 
 torch.manual_seed(0)
-x_best, y_best = gopt.perturbed_gradient_descent(0.0001, 1, 1000, 1000)
+x_best, y_best = gopt.perturbed_gradient_descent(0.01, 1, 1000, 1)
 
 print("x_best ", x_best)
 print("y_best ", y_best)
@@ -23,7 +23,7 @@ print("y_best ", y_best)
 print("modified perturbed gradient descent")
 
 torch.manual_seed(0)
-x_best, y_best = gopt.modified_gradient_descent(0.0001, 1, 1000, 1000, 10)
+x_best, y_best = gopt.modified_gradient_descent(0.01, 1, 1000, 1, .01)
 
 print("x_best ", x_best)
 print("y_best ", y_best)
